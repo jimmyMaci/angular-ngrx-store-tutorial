6366d41bb96d0a9d78d7c7989734576523e2db43
jimmyMaci <tyler.durden69@gmx.de>
2021-10-25 13:45:21 +0200

    3 files modified, 13 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { StoreModule } from '@ngrx/store';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, 
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),],
+  declarations: [AppComponent, BookListComponent, BookCollectionComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
